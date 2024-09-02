72a8e8e474f6cbef7825d29231cba7225ab7da40
Amarnath Tripathy <114403813+amarrr11@users.noreply.github.com>
2024-09-02 12:11:55 +0000

    new file:   01_basics/02_datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+//ecma documentation
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean =true/false
+//null => standalone value
+// undefined => let state;
+//symbol => uniqueness find k lye will use in react
+
+//object
+
+console.log(typeof 44)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+ 
